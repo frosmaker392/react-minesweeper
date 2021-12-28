1a845f788eaf1c0a07c6c1993a0b1f757b4415ab
Ahmad Zhafir <zzzhafir75@gmail.com>
2021-12-28 15:10:09 +0100

    Add main header and footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,18 @@ import './App.css';
 import Game from './components/Game';
 
 function App() {
-  return <Game />;
+  return (
+    <>
+      <header>
+        <h1>Minesweeper</h1>
+      </header>
+      <Game />
+      <footer className="footer">
+        <p>Hastily written by <a href="https://github.com/frosmaker392">frosmaker392</a>. <a href="https://github.com/frosmaker392/react-minesweeper">Source code</a>.
+        </p>
+      </footer>
+    </>
+  );
 }
 
 export default App;
